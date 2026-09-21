3301fe0fb334b838ffab97e4e52e6945ddc1b916
cchihoangg <chihoang.fashiontech@gmail.com>
2026-09-21 03:44:48 -0500

    Delete ballast directory

--- ballast/js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-const FIREBASE_CONFIG = {
- apiKey: "AIzaSyDO9TluDH8tB61wnNV_c-gFSVkHAX5Usbg",
-  authDomain: "ballast-2dc1f.firebaseapp.com",
-  projectId: "ballast-2dc1f",
-  storageBucket: "ballast-2dc1f.firebasestorage.app",
-  messagingSenderId: "866578624166",
-  appId: "1:866578624166:web:f7a9429adb11be1a794e67"
-};
